5969ce5c57b443077e24b86f8d5c07a9c2b0326d
3562Nathan <nathangaffley@gmail.com>
2016-07-23 23:29:34 +0200

    Auto stash for cherrypick of "Added package.json with all necessary dependencies."
    Added watcher and other concurrency functionality.

--- server.babel.js
+++ server.babel.js
@@ -1,5 +1,6 @@
-import express from express
+import express from 'express';
 const app = express();
 
 app.use('/', express.static('public'));
 app.listen(process.env.PORT || 3000);
+console.log("server started...");
